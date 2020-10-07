b230b654d15a6b9b0960cb83e16e0d6265fc8332
mete23 <67523122+mete23@users.noreply.github.com>
2020-10-07 17:54:38 +0200

    Update JavaLoops2.java
    This version is much shorter and easier to understand!

--- JavaHackerrank/JavaLoops2.java
+++ JavaHackerrank/JavaLoops2.java
@@ -1,39 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-class JavaLoops2 {
-    public static void main(String[] argh) {
+class JavaLoops2{
+    public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for (int i = 0; i < t; i++) {
+        int q=in.nextInt();
+        for(int i=0;i<q;i++){
+            
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
-            printAP(a, b, n);
+            for(int t=0;t<n;t++) {
+                a=a+b;
+                System.out.print(a + " ");
+                b*=2;
+            }
+            System.out.println();
         }
         in.close();
-    }
-
-    private static void printAP(int a, int b, int n) {
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int sumOfPowers = calculatePower(i, b);
-            arr[i] = a + sumOfPowers;
-            if (i < n - 1) {
-                System.out.print(arr[i] + " ");
-            } else {
-                System.out.println(arr[i]);
-            }
-
-        }
-    }
-
-    private static int calculatePower(int i, int b) {
-        int sum = 0;
-        for (int j = 0; j <= i; j++) {
-            int oneValue = (int) (Math.pow(2, j)) * b;
-            sum = sum + oneValue;
-        }
-        return sum;
     }
 }
